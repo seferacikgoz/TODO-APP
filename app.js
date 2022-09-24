53230d66ab509af81f49fee1a2d4f592490dee37
sefer <seferackgz@gmail.com>
2022-09-24 11:30:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//* ================================================ */
+//*                 TODO APP
+//* ================================================ */
+
+const addBtn = document.getElementById("todo-button")
+const todInput = document.getElementById("todo-input")
+const todoUl = document.getElementById("todo-ul")
+
+ 
