487622a1c12a7efd25245bdb6ca51d5d4d8ef95b
sefer <seferackgz@gmail.com>
2022-09-26 00:06:28 +0300

    v-2

--- app.js
+++ app.js
@@ -2,8 +2,20 @@
 //*                 TODO APP
 //* ================================================ */
 
+//? Selectors
 const addBtn = document.getElementById("todo-button")
-const todInput = document.getElementById("todo-input")
+const todoInput = document.getElementById("todo-input")
 const todoUl = document.getElementById("todo-ul")
 
- 
+addBtn.addEventListener("click", () => {
+    if(todoInput.value.trim() === ""){
+        alert("Please enter new todo")
+    }else {
+    const newTodo = {
+        id: new Date().getTime(),
+        completed:false,
+        text:todoInput.value,
+    }
+    }
+    
+})
